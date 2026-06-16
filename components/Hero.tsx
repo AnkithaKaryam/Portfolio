8c1d17d3f7fc3439ca56ca8af0347df8345e5b07
Ankitha Karyam <ankithakaryam.w@gmail.com>
2026-06-16 20:51:01 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -63,18 +63,18 @@ const Hero = () => {
                   href="mailto:ankitha.karyam.work@gmail.com"
                   className="hover:text-terminal-white transition-colors duration-300"
                 >
-                  <span className="text-terminal-gray5"> > </span>
+                  <span className="text-terminal-gray5"> {'>'} </span>
                   ankitha.karyam.work@gmail.com
                 </a>
                 <a
                   href="tel:+918074543716"
                   className="hover:text-terminal-white transition-colors duration-300"
                 >
-                  <span className="text-terminal-gray5"> > </span>
+                  <span className="text-terminal-gray5"> >{'>'}</span>
                   +91 8074543716
                 </a>
                 <span>
-                  <span className="text-terminal-gray5"> > </span>
+                  <span className="text-terminal-gray5"> {'>'} </span>
                   Bangalore, India
                 </span>
               </div>
